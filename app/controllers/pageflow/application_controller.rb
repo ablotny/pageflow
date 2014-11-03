aa096cf8cccc39833518ca5bc1edd987ae22baa6
Blotny, Alexander <alexander.blotny@asideas.de>
2014-11-03 11:18:10 +0100

    Removed setting of cache headers

--- app/controllers/pageflow/application_controller.rb
+++ app/controllers/pageflow/application_controller.rb
@@ -8,7 +8,6 @@ module Pageflow
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
-    before_filter :customheaders
 
     include EditLocking
 
@@ -55,13 +54,6 @@ module Pageflow
       if request.ssl?
         redirect_to("http://#{request.host}#{request.fullpath}", :status => :moved_permanently)
       end
-    end
-
-    private
-
-    def customheaders
-      response.headers["Cache-Control"]= ENV["CACHE_CONTROL"]
-      response.headers["Server"]= ""
     end
   end
 end
